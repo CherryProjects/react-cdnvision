d419b858b63d9998939950ed12eabba98dc8ba51
Zlatko Fedor <zfedor@gmail.com>
2016-04-28 23:15:13 +0200

    deps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Img from './Img';
 import Cover from './Cover';
 import Provider from './Provider';
+import { Limit, Fit, Gravity } from 'cdnvision';
 
+export { Limit, Fit, Gravity };
 export { Img, Cover, Provider };
 export default Provider;
