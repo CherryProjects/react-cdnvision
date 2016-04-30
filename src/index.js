416ff92e64f4a95fda9d1293da24739d94ca724e
Zlatko Fedor <zfedor@gmail.com>
2016-04-30 23:21:09 +0200

    Release 0.1.11

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import Img from './Img';
 import Cover from './Cover';
 import Provider from './Provider';
 import { Limit, Fit, Gravity } from 'cdnvision';
+import provideGetImageURL from './provideGetImageURL';
 
 export { Limit, Fit, Gravity };
 export { Img, Cover, Provider };
+export { provideGetImageURL };
 export default Provider;
