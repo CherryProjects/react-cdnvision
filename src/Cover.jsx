156280df79a9ce6a10f4709519a5ff2644e9904c
Zlatko Fedor <zfedor@gmail.com>
2016-07-10 18:30:13 +0200

    Release 0.1.18

--- src/Cover.jsx
+++ src/Cover.jsx
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import Provider from './Provider';
 
 const defaultStyle = {
@@ -6,41 +6,39 @@ const defaultStyle = {
   backgroundPosition: 'center',
 };
 
-export default class Cover extends Component {
-  static contextTypes = {
-    ...Provider.childContextTypes,
+export default function Cover(props, context) {
+  const { style, className, src, children, options = {}, width, height } = props;
+  const { cdnVisionClient } = context;
+  const url = cdnVisionClient.getImageURL({
+    width,
+    height,
+    ...options,
+    path: src,
+  });
+
+  const finallStyle = {
+    ...defaultStyle,
+    ...style,
+    backgroundImage: url,
   };
 
-  static propTypes = {
-    src: PropTypes.string.isRequired,
-    className: PropTypes.string,
-    style: PropTypes.object,
-    options: PropTypes.object,
-    children: PropTypes.node,
-    width: PropTypes.number,
-    height: PropTypes.number,
-  };
-
-  render() {
-    const { style, className, src, children, options = {}, width, height } = this.props;
-    const { cdnVisionClient } = this.context;
-    const url = cdnVisionClient.getImageURL({
-      width,
-      height,
-      ...options,
-      path: src,
-    });
-
-    const finallStyle = {
-      ...defaultStyle,
-      ...style,
-      backgroundImage: url,
-    };
-
-    return (
-      <div style={finallStyle} className={className}>
-        {children}
-      </div>
-    );
-  }
+  return (
+    <div style={finallStyle} className={className}>
+      {children}
+    </div>
+  );
 }
+
+Cover.contextTypes = {
+  ...Provider.childContextTypes,
+};
+
+Cover.propTypes = {
+  src: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  style: PropTypes.object,
+  options: PropTypes.object,
+  children: PropTypes.node,
+  width: PropTypes.number,
+  height: PropTypes.number,
+};
--- src/Img.jsx
+++ src/Img.jsx
@@ -1,67 +1,81 @@
-import React, { Component, PropTypes } from 'react';
-import Provider from './Provider';
+import React, { PropTypes } from 'react';
 import Client from 'cdnvision';
 
-export default class Img extends Component {
-  static contextTypes = {
-    cdnVisionClient: PropTypes.object,
+function clearProps(props) {
+  const newProps = {
+    ...props,
   };
 
-  static propTypes = {
-    src: PropTypes.string.isRequired,
-    alt: PropTypes.string,
-    width: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number,
-    ]),
-    height: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number,
-    ]),
-    quality: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number,
-    ]),
-    options: PropTypes.object,
+  delete newProps.width;
+  delete newProps.height;
+  delete newProps.options;
+  delete newProps.name;
+  delete newProps.gravity;
+  delete newProps.fit;
+  delete newProps.limit;
+  delete newProps.quality;
 
-    gravity: PropTypes.string,
-    fit: PropTypes.string,
-    limit: PropTypes.string,
-
-    name: PropTypes.string,
-    protocol: PropTypes.string,
-  };
-
-  render() {
-    const { src, alt, options = {}, width, height, name, protocol, gravity, fit, limit, quality } = this.props;
-
-    const cdnVisionClient = name
-      ? new Client({ name, protocol })
-      : this.context.cdnVisionClient;
-
-    const url = cdnVisionClient.getImageURL({
-      width,
-      height,
-      gravity,
-      fit,
-      limit,
-      quality,
-      ...options,
-      path: src,
-    });
-
-    const imgProps = {
-      ...this.props,
-      width: void 0,
-      height: void 0,
-      options: void 0,
-      name: void 0,
-      gravity: void 0,
-      fit: void 0,
-      limit: void 0,
-      quality: void 0,
-    };
-
-    return <img {...imgProps} src={url} alt={alt} />;
-  }
+  return newProps;
 }
+
+export default function Img(props, context) {
+  const {
+    src,
+    alt,
+    options = {},
+    width,
+    height,
+    name,
+    protocol,
+    gravity,
+    fit,
+    limit,
+    quality,
+  } = props;
+
+  const cdnVisionClient = name
+    ? new Client({ name, protocol })
+    : context.cdnVisionClient;
+
+  const url = cdnVisionClient.getImageURL({
+    width,
+    height,
+    gravity,
+    fit,
+    limit,
+    quality,
+    ...options,
+    path: src,
+  });
+
+  return <img {...clearProps(props)} src={url} alt={alt} />;
+}
+
+Img.contextTypes = {
+  cdnVisionClient: PropTypes.object,
+};
+
+Img.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string,
+  width: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+  height: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+  quality: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+  options: PropTypes.object,
+
+  gravity: PropTypes.string,
+  fit: PropTypes.string,
+  limit: PropTypes.string,
+
+  name: PropTypes.string,
+  protocol: PropTypes.string,
+};
